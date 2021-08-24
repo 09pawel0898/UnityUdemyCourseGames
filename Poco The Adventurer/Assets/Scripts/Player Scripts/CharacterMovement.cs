b6daa6e9b148ee32d7d77d1e21740ea101a8aa83
09pawel0898 <pawel_2014@interia.eu>
2021-08-24 17:13:25 +0200

    joystick assets added

--- Poco The Adventurer/Assets/Scripts/Player Scripts/CharacterMovement.cs
+++ Poco The Adventurer/Assets/Scripts/Player Scripts/CharacterMovement.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] protected float xSpeed = 1f, ySpeed = 0.75f;
+
+    private Vector3 moveDelta;
+    private RaycastHit2D movementHit;
+    private BoxCollider2D myCollider;
+    public bool HasPlayerTarget{ get;set; }
+
+    protected virtual void Awake()
     {
-        
+        myCollider = GetComponent<BoxCollider2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected virtual void HandleMovement(float x, float y)
     {
-        
+        moveDelta = new Vector3(x * xSpeed, y * ySpeed, 0f);
+        movementHit = Physics2D.BoxCast(transform.position, 
+                                        myCollider.size, 0f, 
+                                        new Vector2(0f, moveDelta.y), 
+                                        Mathf.Abs(moveDelta.y * Time.deltaTime), 
+                                        LayerMask.GetMask(TagManager.BLOCKING_LAYER_MASK));
+
+        if(movementHit.collider == null) // not colliding with any object on blocking layer
+            transform.Translate(0f, moveDelta.y * Time.deltaTime, 0f);
+
+        movementHit = Physics2D.BoxCast(transform.position,
+                                       myCollider.size, 0f,
+                                       new Vector2(moveDelta.x, 0),
+                                       Mathf.Abs(moveDelta.x * Time.deltaTime),
+                                       LayerMask.GetMask(TagManager.BLOCKING_LAYER_MASK));
+
+        if (movementHit.collider == null)
+            transform.Translate(moveDelta.x * Time.deltaTime, 0f, 0f);
+
     }
+
+    public Vector3 GetMoveDelta()
+    {
+        return moveDelta;
+    }
+
 }
